91da3dbd6a0c6b190173ce2b577be409d11ac82e
Lisandro Luna <18648174+LisandroLuna@users.noreply.github.com>
2024-07-14 16:04:18 -0300

    Server side scrapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import ProductInfo from './components/ProductInfo';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -11,13 +12,29 @@ interface Product {
   }[];
 }
 
-export default function Home() {
+async function fetchPrices(products: Product[]) {
+  const res = await fetch('http://localhost:3000/api/fetchPrices', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(products),
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch prices');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
   const products: Product[] = [
     {
       name: 'Coca Cola 2.25Lts',
       urls: [
-        { url: 'https://www.cotodigital3.com.ar/sitios/cdigi/producto/-gaseosa-coca-cola-sabor-original--225-lt/_/A-00014450-00014450-200', selector: '.price_regular_precio' },
-        { url: 'https://www.masonline.com.ar/gaseosa-coca-cola-sabor-original-2-25-lt/p', selector: '.valtech-gdn-dynamic-product-0-x-dynamicProductPrice' },
+        //{ url: 'https://www.cotodigital3.com.ar/sitios/cdigi/producto/-gaseosa-coca-cola-sabor-original--225-lt/_/A-00014450-00014450-200', selector: '.price_regular_precio' },
+        { url: 'https://www.masonline.com.ar/gaseosa-coca-cola-sabor-original-2-25-lt/p', selector: '.valtech-gdn-dynamic-product-0-x-dynamicProductPrice mb4' },
         { 
           url: 'https://diaonline.supermercadosdia.com.ar/gaseosa-coca-cola-sabor-original-225-lt-14837/p', 
           selector: '.vtex-product-price-1-x-currencyContainer', 
@@ -32,23 +49,27 @@ export default function Home() {
     },
   ];
 
+  const results = await fetchPrices(products);
+
   return (
     <div className="min-h-screen">
-        <nav className="w-full grid items-center grid-cols-3 border-b border-neutral-300 h-20 shadow-sm relative">
-          <Link className="col-start-2 col-end-3 justify-self-center" href="/">
-            <Image className="col-start-2 col-end-3 justify-self-center"
-              src="/CocaHoy.png" // Path to the image in the public folder
-              alt="Precio de la Coca Cola de 2.25Lts"
-              width={200}  // Specify the width of the image
-              height={100} // Specify the height of the image
-            />
-          </Link>
-        </nav>
-        <main className="p-4">      
-            {products.map((product, index) => (
-              <ProductInfo key={index} name={product.name} urls={product.urls} />
-            ))}
-        </main>
-  </div>
+      <nav className="w-full grid items-center grid-cols-3 border-b border-neutral-300 h-20 shadow-sm relative">
+        <Link className="col-start-2 col-end-3 justify-self-center" href="/">
+          <Image className="col-start-2 col-end-3 justify-self-center"
+            src="/CocaHoy.png"
+            alt="Precio de la Coca Cola de 2.25Lts"
+            width={200}
+            height={100}
+          />
+        </Link>
+      </nav>
+      <main className="p-4">      
+        <Suspense fallback={<div>Loading...</div>}>
+          {results.map((result, index) => (
+            <ProductInfo key={index} {...result} />
+          ))}
+        </Suspense>
+      </main>
+    </div>
   );
 }
